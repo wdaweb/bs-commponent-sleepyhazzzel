eae76a5da123e80784fca8a7973b30055cde0948
sleepyhazzzel <yangching.emily@gmail.com>
2024-01-09 04:16:20 +0800

    add #work hover

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-for (let i = 1; i <= 8; i++) {
+var hoverText = [
+  { title: 'Clean Divice Display', tools: 'illustrator / Photoshop' },
+  { title: 'Clean Watch', tools: 'Client TreeThemes' },
+  { title: 'Apple Iwatch', tools: 'illustrator / Photoshop' },
+  { title: 'Apple IMac', tools: 'Mockup / Photoshop' },
+  { title: 'Single Images', tools: 'Photography' },
+  { title: 'Especial Coffee Cup', tools: 'Pagination / InDesign' },
+  { title: 'Black Iphone', tools: 'Wordpress / Photoshop' },
+  { title: 'Fullscreen Video', tools: 'Photography / Video' }
+]
+
+for (let i = 0; i < 8; i++) {
   $('#work-img').append(`
   <div class="col">
-    <img src="./img/SingleProjectAjax${i}.jpg">
+    <img src="./img/SingleProjectAjax${i + 1}.jpg">
+    <div class="hover-black">
+      <h6 class="hover-title">${hoverText[i].title}</h6>
+      <p class="hover-tools">${hoverText[i].tools}</p>
+    </div>
   </div>`
   )
 }
